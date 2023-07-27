397f51eb2bae3048ac4a788258c6e22bad847347
abdulaamir9496 <abdulaamir9496@gmail.com>
2023-07-28 02:03:17 +0530

    initial

---
+++ src/DSA_2D_Array_SumitMalik/Introduction_2DArray.java
@@ -0,0 +1,24 @@
+package DSA_2D_Array_SumitMalik;
+
+import java.util.Scanner;
+
+public class Introduction_2DArray {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
