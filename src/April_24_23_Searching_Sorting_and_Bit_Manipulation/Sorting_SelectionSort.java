68a16751ee71e0341bb47abed7fbbd64b0c09516
abdulaamir9496 <abdulaamir9496@gmail.com>
2023-08-06 20:16:58 +0530

    initial

--- src/April_24_23_Searching_Sorting_and_Bit_Manipulation/Sorting_SelectionSort.java
+++ src/April_24_23_Searching_Sorting_and_Bit_Manipulation/Sorting_SelectionSort.java
@@ -1,34 +1,34 @@
 package April_24_23_Searching_Sorting_and_Bit_Manipulation;
 import java.util.Arrays;
 /*
-Approach: Insertion Sort
+Approach: Selection Sort
 Time Complexity: O(n^2)
 Space Complexity: O(1)
- */
-public class Sorting_InsertionSort {
-    //Implementation of insertion order
-    public static void insertionOrder(int[] arr)
-    {
-        for (int i = 0; i < arr.length; i++) {
+ */     
+public class Sorting_SelectionSort {
+    public static void selectionSort(int[] arr) {
+        //Implementation of insertion sort
+        for (int i = 1; i < arr.length; i++) {
             int j = i;
-            while (j > 0 && arr[j] < arr[j-1])
-            {
-                //Swap between the elements arr[j] and arr[j-1]
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                //Swap the elements between the arr[j] and arr[j-1]
                 int temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
-                
+
                 j--;
             }
         }
     }
+
     public static void main(String[] args) {
         //Defining array
         int[] arr = {20, 50, 25, 67, 79, 12, 15};
-        
+
         //Function calling
-        insertionOrder(arr);
-        System.out.println("Insertion order is: ");
+        selectionSort(arr);
+
+        System.out.println("Sorted array is: ");
         System.out.println(Arrays.toString(arr));
     }
 }
