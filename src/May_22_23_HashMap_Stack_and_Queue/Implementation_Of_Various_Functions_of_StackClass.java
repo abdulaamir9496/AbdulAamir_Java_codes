a2b1132c2190658106774a6aecabc547a2a6d0be
abdulaamir9496 <abdulaamir9496@gmail.com>
2023-09-17 21:16:55 +0530

    initial

--- src/May_22_23_HashMap_Stack_and_Queue/Implementation_Of_Various_Functions_of_StackClass.java
+++ src/May_22_23_HashMap_Stack_and_Queue/Implementation_Of_Various_Functions_of_StackClass.java
@@ -13,11 +13,19 @@ public class Implementation_Of_Various_Functions_of_StackClass {
         stack.push(9);
         
         //Print the topmost element from the stack
-        System.out.println("Top most element in the stack: " + stack.peek());
+        System.out.println("Top most element in the stack before deletion is: " + stack.peek());
         
         //Delete the top most element from the stack
         System.out.println("Deleted the top most element from the stack: " + stack.pop());
 
-        System.out.println("Current top most element in the stack is: " + stack.peek());
+        System.out.println("Current top most element in the stack after deletion is: " + stack.peek());
+        
+        //Searching the element in the stack
+        //Search function returns -1 when the element is not present in the stack
+        System.out.println(stack.search("Element 1 is present at the position: " + stack.search(1)));
+        
+        //Check whether the stack is empty or not?
+        //Empty function returns the boolean value(True/ False)
+        System.out.println("Is stack empty or not: " + stack.isEmpty());
     }
 }
